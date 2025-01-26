3512997cefb2ad95a9194c80adc568e9bf8d6cdc
joaquin-daruich <joaquindaruichh@gmail.com>
2025-01-26 12:50:51 -0300

    trabajo-wp

--- src/componentes/Inicio.jsx
+++ src/componentes/Inicio.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
- import { MensajesLista } from '../ListaDeMensajes';
+import { MensajesLista } from '../ListaDeMensajes';
 import { useGlobalContext } from './GlobalContext';
+
 
 
 
