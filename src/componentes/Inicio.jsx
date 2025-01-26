a9b405cca6c536fe944b6b1591c0866b60f1fe54
joaquin-daruich <joaquindaruichh@gmail.com>
2025-01-26 14:49:19 -0300

    carl

--- src/componentes/Inicio.jsx
+++ src/componentes/Inicio.jsx
@@ -36,7 +36,7 @@ const {mostrarEnamorado} = useGlobalContext()
 
             <h1 className='elegir'>Elegí mas!</h1>
             <Link to={'/crearNuevaCarta'}>
-            <img  className='tarot' src="../../public/completo.jpg" alt="" />
+            <img  className='tarot' src="/completo.jpg" alt="" />
             </Link>
 
     </div>
